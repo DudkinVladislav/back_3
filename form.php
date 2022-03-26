fd674e760ccd0f0454e52033c79fc2004e4acdca
DudkinVladislav <90377440+DudkinVladislav@users.noreply.github.com>
2022-03-26 06:38:26 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,93 @@
+﻿<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+<style>body { margin:0;
+	display:flex;
+	flex-direction:column;
+text-align:center;
+background-color:#ff9911;}
+header {display:flex;
+flex-direction: column;
+text-align: center;
+background-color:#dc3545}</style>
+    <title>Задание 3</title>
+  </head>
+  <body>
+<header>
+<p>Здравствуйте, заполните анкету, чтобы мы могли сохранить ваши данные.</p>
+<p>Пожалуйста, указывайте только достоверную информацию.</p>
+</header>
+	<form action="" method="POST">
+      <label>
+        Имя:
+	<br>
+        <input name="fio" value="Введите имя">
+	</label>
+	<br>
+      	<label>
+         email:<br>
+        <input name="email" value="Введите свой email" type="email">
+     	</label>
+	<br>
+      	<label>
+        Дата рождения:<br>
+        <input name="date" value="2022-01-31" type="date">
+      	</label>
+	<br>
+	Пол:
+	<br> 
+      	<label>
+	  <input type="radio" checked="checked" name="pol" value="woman">
+        Женский
+	</label>
+	<br>
+        <label>
+	  <input type="radio" name="pol" value="man">
+        Мужской
+	</label>
+	<br>
+	Количество конечностей:
+	<br>
+	<label>
+	  <input type="radio" checked="checked" name="parts" value="1">
+        1 конечность
+	</label>
+	<label>
+	  <input type="radio" checked="checked" name="parts" value="2">
+        2 конечности
+	</label>
+	<label>
+	  <input type="radio" checked="checked" name="parts" value="3">
+        3 конечности
+	</label>
+	<label>
+	  <input type="radio" name="parts" value="4">
+        4 конечности
+	</label>
+	<br>
+     	<label>
+	Сверхспособности:
+	<br>
+        <select name="abilities[]" multiple="true">
+          <option value="immortality">Бессмертие</option>
+          <option value="intangibility">Прохождение сквозь стены</option>
+          <option value="levitation">Левитация</option>
+        </select>
+      	</label>
+	<br>
+	<label>
+	Биография:
+	<br>
+        <textarea name="biography" style="margin: 0px; height: 69px; width: 180px;">Расскажите о своей жизни</textarea>
+      	</label>
+	<br>
+      	<label>
+	<input type="checkbox" checked="checked" name="check">
+       С контрактом ознакомлен(а)
+	</label>
+	<br>
+      	<input type="submit" value=Отправить>
+    </form>
+<img src="./img/create.png" alt="creation">
+<img src="./img/rows.png" alt="rows">
+</body></html>
---
+++ index.php
@@ -0,0 +1,78 @@
+﻿<?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+  if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    print('Спасибо, результаты сохранены.');
+exit();
+  }
+  // Включаем содержимое файла form.php.
+  include('form.php');
+  // Завершаем работу скрипта.
+  exit();
+}
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
+// Проверяем ошибки.
+$errors = FALSE;
+if (empty($_POST['fio'])) {
+  print('Заполните имя.<br/>');
+  $errors = TRUE;
+}
+if (empty($_POST['email'])) {
+  print('Заполните email.<br/>');
+  $errors = TRUE;
+}
+if (empty($_POST['date'])) {
+  print('Заполните дату.<br/>');
+  $errors = TRUE;
+}
+if (empty($_POST['biography'])) {
+  print('Расскажите о себе.<br/>');
+  $errors = TRUE;
+}
+// *************
+// Тут необходимо проверить правильность заполнения всех остальных полей.
+// *************
+
+if ($errors) {
+  // При наличии ошибок завершаем работу скрипта.
+  exit();
+}
+
+$fio=$_POST['fio'];
+$email=$_POST['email'];
+$date=$_POST['date'];
+$bio=$_POST['biography'];
+$pol=$_POST['pol'];
+$parts=$_POST['parts'];
+$powers= implode(",",$_POST['abilities']);
+// Сохранение в базу данных.
+
+$user = 'u46613';
+$pass = '1591065';
+$db = new PDO('mysql:host=localhost; dbname=u46613', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+// Подготовленный запрос. Не именованные метки.
+try {
+  $stmt = $db->prepare("INSERT INTO application SET name = ?, email = ?, date=?, pol= ?, parts= ?, abilities= ?, bio= ?");
+  $stmt -> execute([$_POST['fio'], $_POST['email'], $_POST['date'], $_POST['pol'], $_POST['parts'], $powers, $_POST['biography']]);
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+
+//  stmt - это "дескриптор состояния".
+
+// Делаем перенаправление.
+// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
+// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
+header('Location: ?save=1');
